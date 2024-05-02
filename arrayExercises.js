7b25518066e9f3ea25678311e578f486982939df
ilkin-ibadov <ilkin.ibadov21@gmail.com>
2024-05-02 23:35:56 +0400

    all

---
+++ arrayExercises.js
@@ -0,0 +1,15 @@
+// Write a function that takes an array and returns a new array with the last item removed.
+
+// Write a function that takes an array and returns the length of items it has.
+
+// Write a function that takes an array and an item, returns the array with the item added to the end of array.
+
+// Write a function that takes an array and an item, returns the array with the item added to the beginning of array.
+
+// Write a function that takes an array of words and returns a string of the words seperated by ,
+
+// Write a function that takes an array and two indexes (start and end). Return an array consisting of items between the given indexes.
+
+// Write a function that takes an array and an index, returns the item at given index.
+
+// Write a function that takes an array and an item, removes that item from the array without leaving undefined items, returns the new array.
---
+++ stringExercises.js
@@ -0,0 +1,29 @@
+// Exercise 1: String Length 
+// Write a function that takes a string and returns its length.
+
+// Exercise 2: Character at Index 
+// Write a function that takes a string and an index. Return the character at that index.
+
+// Exercise 3: Extract Substring 
+// Write a function that takes a string and two indices (start and end). Return the substring between the given indices.
+
+// Exercise 4: Split and Count Words 
+// Write a function that takes a sentence and returns the number of words in the sentence.
+
+// Exercise 5: Search and Replace 
+// Write a function that takes a sentence and allows the user to search for a word or phrase and replace it with another word or phrase.
+
+// Exercise 6: Find Index of Character 
+// Write a function that takes a string and a character and returns the index of the first occurrence of that character in the string.
+
+// Exercise 7: Reverse a String 
+// Write a function that reverses and returns a given string.
+
+// Exercise 8: Remove Whitespace 
+// Write a function that takes a sentence with extra whitespace, removes the extra spaces and returns the sentence.
+
+// Exercise 9: Concat
+// Write a function that concatenates two given strings and returns the resulting string.
+
+// Exercise 10: Uppercase Conversion 
+// Write a function that takes a sentence and converts the first letter of each word to uppercase.
