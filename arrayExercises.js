d9f390f5ec4977ccc836f238fed9f4ddb6551fe1
Parvin Agayev <a.parvin@ultra.az>
2024-05-03 08:09:04 +0400

    finished

--- arrayExercises.js
+++ arrayExercises.js
@@ -1,15 +1,37 @@
 // Write a function that takes an array and returns a new array with the last item removed.
-
+function task1(arr) {
+    arr.pop();
+    return arr;
+}
 // Write a function that takes an array and returns the length of items it has.
-
+function task2(arr){
+    return arr.length;
+}
 // Write a function that takes an array and an item, returns the array with the item added to the end of array.
+function task3(arr,item){
+    arr.push(item);
+    return arr;
+}
 
 // Write a function that takes an array and an item, returns the array with the item added to the beginning of array.
-
+function task4(arr,item){
+    arr.unshift(item);
+    return arr;
+}
 // Write a function that takes an array of words and returns a string of the words seperated by ,
-
+function task5(arr){
+    return arr.join(', ');
+}
 // Write a function that takes an array and two indexes (start and end). Return an array consisting of items between the given indexes.
-
+function task6(arr, start, end) {
+    return arr.slice(start, end + 1);
+}
 // Write a function that takes an array and an index, returns the item at given index.
-
+function task7(arr, index) {
+    return arr[index];
+}
 // Write a function that takes an array and an item, removes that item from the array without leaving undefined items, returns the new array.
+function task8(arr, item) {
+    return arr.filter(element => element !== item);
+}
+
--- stringExercises.js
+++ stringExercises.js
@@ -1,29 +1,62 @@
 // Exercise 1: String Length 
 // Write a function that takes a string and returns its length.
 
-// Exercise 2: Character at Index 
+function task1(str) {
+    return str.length;
+}
+
+// Exercise 2: Character at Index
 // Write a function that takes a string and an index. Return the character at that index.
+function task2(str, index) {
+    return str[index];
+}
 
-// Exercise 3: Extract Substring 
+// Exercise 3: Extract Substring
 // Write a function that takes a string and two indices (start and end). Return the substring between the given indices.
-
-// Exercise 4: Split and Count Words 
+function task3(str, start, end) {
+    return str.substring(start, end);
+}
+// Exercise 4: Split and Count Words
 // Write a function that takes a sentence and returns the number of words in the sentence.
+function task4(str) {
+    return str.split(" ").length;
+}
 
-// Exercise 5: Search and Replace 
+// Exercise 5: Search and Replace
 // Write a function that takes a sentence and allows the user to search for a word or phrase and replace it with another word or phrase.
+function task5(sentence, searchWord, replaceWord) {
+    return sentence.replace(searchWord, replaceWord);
+}
 
-// Exercise 6: Find Index of Character 
+// Exercise 6: Find Index of Character
 // Write a function that takes a string and a character and returns the index of the first occurrence of that character in the string.
+function task6(str, char) {
+    return str.indexOf(char);
+}
 
-// Exercise 7: Reverse a String 
+// Exercise 7: Reverse a String
 // Write a function that reverses and returns a given string.
-
-// Exercise 8: Remove Whitespace 
+function task7(str) {
+    return str.split('').reverse().join('');
+}
+// Exercise 8: Remove Whitespace
 // Write a function that takes a sentence with extra whitespace, removes the extra spaces and returns the sentence.
+function task8(str) {
+    return str.trim();
+}
 
 // Exercise 9: Concat
 // Write a function that concatenates two given strings and returns the resulting string.
+function task9(str1, str2) {
+    return str1.concat(str2);
+}
 
-// Exercise 10: Uppercase Conversion 
+// Exercise 10: Uppercase Conversion
 // Write a function that takes a sentence and converts the first letter of each word to uppercase.
+function task10(sentence) {
+    var words = sentence.split(' ');
+    words.forEach((word,index) => {
+        words[index] = word.charAt(0).toUpperCase() + word.slice(1)
+    });
+    return words.join(' ');
+}
